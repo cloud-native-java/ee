394b50eac750c198b60020b1df186924170756bb
Josh Long <josh@joshlong.com>
2015-08-06 22:34:40 -0700

    initial commit of REST content

---
+++ bootiful-rest-basics/src/main/java/demo/Customer.java
@@ -0,0 +1,51 @@
+package demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id ;
+
+    private String firstName, lastName ;
+
+    public Customer (Long id , String f, String l){
+        this.id = id;
+        this.firstName = f;
+        this.lastName = l ;
+    }
+
+    public Customer() {
+    }
+
+    public Customer(String firstName,
+                    String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
