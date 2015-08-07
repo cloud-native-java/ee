f731edf819d14c4d62e8e617949b3597919ce9f2
Josh Long <josh@joshlong.com>
2015-08-06 23:05:38 -0700

    initial commit

---
+++ src/main/java/demo/GreetingMessageProcessor.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.jms.annotation.JmsListener;
+
+import javax.inject.Named;
+import javax.jms.JMSException;
+
+@Named // <1>
+public class GreetingMessageProcessor {
+
+    @JmsListener(destination = "greetings")
+    public void processGreeting(Greeting greeting) throws JMSException {
+        System.out.println("received message: " + greeting);
+    }
+}
---
+++ src/main/java/demo/JerseyConfig.java
@@ -0,0 +1,16 @@
+package demo;
+
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import javax.inject.Named;
+
+
+@Named // <1>
+public class JerseyConfig extends ResourceConfig {
+
+    public JerseyConfig() {
+        this.register(GreetingEndpoint.class); // <2>
+        this.register(JacksonFeature.class); // <3>
+    }
+}
