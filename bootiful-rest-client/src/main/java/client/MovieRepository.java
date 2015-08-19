11776365d04f714c7d5b7f97f4397aee48971c4b
Josh Long <josh@joshlong.com>
2015-08-19 12:29:45 -0300

    api discovery

--- bootiful-rest-client/src/main/java/client/MovieRepository.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-
-@RepositoryRestResource
-interface MovieRepository extends JpaRepository<Movie, Long> {
-}
