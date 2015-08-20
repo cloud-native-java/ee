53400ab722631bc79c3cec4e60a455295921d32d
Josh Long <josh@joshlong.com>
2015-08-20 16:09:40 -0300

    adding update script

---
+++ subtree-update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -e
+
+mkdir -p code
+
+cat cnj-modules.txt | while read l ; do
+  echo "adding $l.."
+  #git="git@github.com:cloud-native-java/$l.git"
+  git="https://github.com/cloud-native-java/$l.git"
+  git subtree pull --prefix code/$l $git master --squash
+done
