2bd3c43390d80b039d5f6ee5ebd4013ddd721cd6
Josh Long <josh@joshlong.com>
2015-08-20 15:07:39 -0300

    updated

--- subtree-init.sh
+++ subtree-init.sh
@@ -3,7 +3,7 @@ set -e
 
 mkdir -p code
 
-cat cnj-chapters.txt | while read l ; do
+cat cnj-modules.txt | while read l ; do
   echo "adding $l.."
   #git="git@github.com:cloud-native-java/$l.git"
   git="https://github.com/cloud-native-java/$l.git"
