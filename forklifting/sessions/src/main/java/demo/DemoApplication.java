47cd3c42b4c9b066243a62d297f6243a0fa121d4
Josh Long <josh@joshlong.com>
2015-08-20 16:19:37 -0300

    Add 'code/forklifting/' from commit 'f063de423ca42e8129231e5c659ae7f32bda0a92'
    git-subtree-dir: code/forklifting
    git-subtree-mainline: a3cdf2e509af352576d1167853b14e6a43dc3191
    git-subtree-split: f063de423ca42e8129231e5c659ae7f32bda0a92

---
+++ forklifting/sessions/src/main/java/demo/DemoApplication.java
@@ -0,0 +1,44 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
+
+@RestController
+class SessionController {
+
+    @Value("${CF_INSTANCE_IP:127.0.0.1}")
+    private String ip;
+
+    @RequestMapping("/hi")
+    Map<String, String> uid(HttpSession session) {
+        // <1>
+        UUID uid = (UUID) session.getAttribute("uid");
+        if (uid == null) {
+            uid = UUID.randomUUID();
+        }
+        session.setAttribute("uid", uid);
+
+        Map<String, String> m = new HashMap<>();
+        m.put("instance_ip", this.ip);
+        m.put("uuid", uid.toString());
+        return m;
+    }
+
+}
