516d981fb98a07a5367b17981e229008c16e0250
Josh Long <josh@joshlong.com>
2015-08-20 10:14:35 -0300

    Squashed 'code/ee/' content from commit bb9bdd3
    git-subtree-dir: code/ee
    git-subtree-split: bb9bdd3dd037cc8e85c87bb70be6777689501776

---
+++ src/main/java/demo/Greeting.java
@@ -0,0 +1,35 @@
+package demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity // <1>
+public class Greeting implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                '}';
+    }
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public Greeting(String name) {
+        this.message = "Hi, " + name + "!";
+    }
+
+    Greeting() {
+    }
+}
---
+++ src/main/java/demo/GreetingServletInitializer.java
@@ -0,0 +1,13 @@
+package demo;
+
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class GreetingServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(Application.class);
+    }
+}
