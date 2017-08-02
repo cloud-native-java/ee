d136694629077679b3d821343103ec1361d60b8a
myarbrough@oreilly.com <myarbrough@oreilly.com>
2017-08-02 15:22:41 -0700

    Edited code/ee/servlets/src/main/java/servlets/ContainerAnalyzer.java with Atlas code editor

--- servlets/src/main/java/servlets/ContainerAnalyzer.java
+++ servlets/src/main/java/servlets/ContainerAnalyzer.java
@@ -4,9 +4,11 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 //@formatter:off
 import 
-    org.springframework.boot.context.embedded.AbstractEmbeddedServletContainer<?pdf-cr?>Factory;
+    org.springframework.boot.context.embedded.AbstractEmbeddedServletContainer
+    Factory;
 import 
-    org.springframework.boot.context.embedded.ConfigurableEmbeddedServlet<?pdf-cr?>Container;
+    org.springframework.boot.context.embedded.ConfigurableEmbeddedServlet
+    Container;
 import org.springframework.boot.context.embedded
         .EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded
@@ -14,7 +16,8 @@ import org.springframework.boot.context.embedded
 import org.springframework.boot.context.embedded
         .tomcat.TomcatEmbeddedServletContainerFactory;
 import 
-    org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServlet<?pdf-cr?>ContainerFactory;
+    org.springframework.boot.context.embedded.undertow.UndertowEmbeddedServlet
+    ContainerFactory;
 //@formatter:on
 import org.springframework.stereotype.Component;
 
@@ -30,7 +33,8 @@ class ContainerAnalyzer implements EmbeddedServletContainerCustomizer {
 
   // <1>
   AbstractEmbeddedServletContainerFactory base = 
-  AbstractEmbeddedServletContainer<?pdf-cr?>Factory.class
+  AbstractEmbeddedServletContainer
+  Factory.class
    .cast(c);
   this.log.info("the container's running on port " + base.getPort());
   this.log.info("the container's context-path is " + base.getContextPath());
@@ -39,7 +43,8 @@ class ContainerAnalyzer implements EmbeddedServletContainerCustomizer {
   if (UndertowEmbeddedServletContainerFactory.class.isAssignableFrom(c
    .getClass())) {
    UndertowEmbeddedServletContainerFactory undertow = 
-   UndertowEmbeddedServlet<?pdf-cr?>ContainerFactory.class
+   UndertowEmbeddedServlet
+   ContainerFactory.class
     .cast(c);
    undertow.getDeploymentInfoCustomizers().forEach(
     dic -> log.info("undertow deployment info customizer " + dic));
@@ -50,7 +55,8 @@ class ContainerAnalyzer implements EmbeddedServletContainerCustomizer {
   // <3>
   if (TomcatEmbeddedServletContainerFactory.class
    .isAssignableFrom(c.getClass())) {
-   TomcatEmbeddedServletContainerFactory tomcat = TomcatEmbeddedServletContainer<?pdf-cr?>Factory.class
+   TomcatEmbeddedServletContainerFactory tomcat = TomcatEmbeddedServletContainer
+   Factory.class
     .cast(c);
    tomcat.getTomcatConnectorCustomizers().forEach(
     cc -> log.info("tomcat connector customizer " + cc));
@@ -60,7 +66,8 @@ class ContainerAnalyzer implements EmbeddedServletContainerCustomizer {
 
   // <4>
   if (JettyEmbeddedServletContainerFactory.class.isAssignableFrom(c.getClass())) {
-   JettyEmbeddedServletContainerFactory jetty = JettyEmbeddedServletContainer<?pdf-cr?>Factory.class
+   JettyEmbeddedServletContainerFactory jetty = JettyEmbeddedServletContainer
+   Factory.class
     .cast(c);
    jetty.getServerCustomizers().forEach(
     cc -> log.info("jetty server customizer " + cc));
